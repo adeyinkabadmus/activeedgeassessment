8efa8bc9a79d6a3a5e476e0c3ab61f4f021d992f
adeyinkabadmus <adeyinkabadmus@gmail.com>
2024-01-12 10:52:09 +0100

    feat: added page routing for switching between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
-import ArtistList from './pages/allArtists'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import ArtistList from "./pages/allArtists";
 
 function App() {
-  return (
-    <>
-      <ArtistList />
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<div>
+				<Routes>
+					<Route path="/" element={<ArtistList />} />
+					{/* <Route path="/albums" element={<Album />} /> */}
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
